4e508662b295094a2723ad6382e76e927eb68a84
zhangrong <zhangr@zhangrongtekiMacBook-Air.local>
2014-11-09 13:25:36 +0800

    fgdgdgdgdgdg

--- Hello World/Hello World/AppDelegate.m
+++ Hello World/Hello World/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     // aaa
+    // bbb
     return YES;
 }
 
